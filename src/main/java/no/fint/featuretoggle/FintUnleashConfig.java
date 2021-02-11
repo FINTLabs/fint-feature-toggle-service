4db35a0d4bdf4d36afffa499f4de12007f67c9e0
Frode Sjovatsen <frode@fintlabs.no>
2021-02-11 14:21:56 +0100

    Initial commit

---
+++ src/main/java/no/fint/featuretoggle/FintUnleashConfig.java
@@ -0,0 +1,31 @@
+package no.fint.featuretoggle;
+
+import no.finn.unleash.DefaultUnleash;
+import no.finn.unleash.util.UnleashConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FintUnleashConfig {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${fint.unleash.api:https://unleashed-beta.fintlabs.no/api/}")
+    private String unleashApi;
+
+    @Value("${fint.unleash.instance-id}")
+    private String instanceId;
+
+    @Bean
+    public DefaultUnleash unleash() {
+        UnleashConfig config = UnleashConfig.builder()
+                .appName(applicationName)
+                .instanceId(instanceId)
+                .unleashAPI(unleashApi)
+                .build();
+
+        return new DefaultUnleash(config);
+    }
+}
