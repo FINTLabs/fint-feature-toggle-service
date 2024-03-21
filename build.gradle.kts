c8baa1fc6d42abe2be08b4c0cf1b8ab0fb9f4d93
Mattis Bratland <mattis.bratland@novari.no>
2024-03-21 13:30:46 +0100

    Configure ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,100 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	`jvm-test-suite`
-	`maven-publish`
-	alias(libs.plugins.kotlin.jvm)
-	alias(libs.plugins.kotlin.plugin.spring)
-	alias(libs.plugins.spring.boot)
-	alias(libs.plugins.ktlint)
-	alias(libs.plugins.versions)
+    `jvm-test-suite`
+    `maven-publish`
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.versions)
 }
 
 group = "no.fint"
+version = System.getenv("RELEASE_VERSION") ?: "0-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 ktlint {
-	this.version.set(libs.versions.klint)
+    this.version.set(libs.versions.ktlint)
 }
 
 dependencies {
-	implementation(kotlin("stdlib"))
-	implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
 
-	implementation(libs.spring.boot.web)
+    implementation(libs.spring.boot.web)
+    annotationProcessor(libs.spring.boot.configuration.processor)
 
-	implementation(libs.slf4j)
+    implementation(libs.slf4j)
 
 //    Jackson
-	implementation(libs.jackson.module.kotlin)
+    implementation(libs.jackson.module.kotlin)
 
 //    Unleash
-	implementation(libs.unleash)
+    implementation(libs.unleash)
 
 //    Test
-	testImplementation(libs.spring.boot.test)
-	testImplementation(libs.springmockk)
-	testImplementation(libs.mockk)
+    testImplementation(libs.spring.boot.test)
+    testImplementation(libs.springmockk)
+    testImplementation(libs.mockk)
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 testing {
-	suites {
-		val test by getting(JvmTestSuite::class) {
-			useJUnitJupiter()
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
 
-			targets {
-				all {
-					testTask.configure {
-						testLogging {
-							events = setOf(TestLogEvent.SKIPPED, TestLogEvent.FAILED)
-							exceptionFormat = TestExceptionFormat.FULL
-							showCauses = true
-							showExceptions = true
-							showStackTraces = true
-						}
-					}
-				}
-			}
-		}
-	}
+            targets {
+                all {
+                    testTask.configure {
+                        testLogging {
+                            events = setOf(TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+                            exceptionFormat = TestExceptionFormat.FULL
+                            showCauses = true
+                            showExceptions = true
+                            showStackTraces = true
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	testLogging {
-		events = setOf(TestLogEvent.SKIPPED, TestLogEvent.FAILED)
-		exceptionFormat = TestExceptionFormat.FULL
-		showCauses = true
-		showExceptions = true
-		showStackTraces = true
-	}
+    useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+        exceptionFormat = TestExceptionFormat.FULL
+        showCauses = true
+        showExceptions = true
+        showStackTraces = true
+    }
 }
 
-apply( from = "https://raw.githubusercontent.com/FINTLabs/fint-buildscripts/master/reposilite.fint.ga.gradle")
+apply(from = "https://raw.githubusercontent.com/FINTLabs/fint-buildscripts/master/reposilite.fint.ga.gradle")
