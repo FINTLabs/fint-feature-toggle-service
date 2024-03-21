a61fb338ef3910083d287efc2dc9953d8bc8ee2c
Mattis Bratland <mattis.bratland@novari.no>
2024-03-21 19:14:37 +0100

    Remove unused imports

--- src/test/kotlin/no/fint/featuretoggle/ApplicationContextTest.kt
+++ src/test/kotlin/no/fint/featuretoggle/ApplicationContextTest.kt
@@ -2,7 +2,6 @@ package no.fint.featuretoggle
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(classes = [TestApplication::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
--- src/test/kotlin/no/fint/featuretoggle/TestApplicationConfig.kt
+++ src/test/kotlin/no/fint/featuretoggle/TestApplicationConfig.kt
@@ -2,7 +2,6 @@ package no.fint.featuretoggle
 
 import io.getunleash.FakeUnleash
 import io.getunleash.Unleash
-import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
